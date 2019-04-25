8268357bf08f22353005b4c848233dba7e921df0
Sylaina <skie_@web.de>
2019-04-25 14:34:19 +0200

    Update mcpDAC.h

--- mcpDAC.h
+++ mcpDAC.h
@@ -15,30 +15,30 @@ extern "C" {
 
 #include <avr/io.h>
 /**********************************************
-Public Function: mcpADCInit
-
-Purpose: Initialise SPI interface
-
-Input Parameter:
--volatile uint8_t *cPort:  Pointer to PORT-register of CS-pin
--volatile uint8_t *cDDR:   Pointer to DDR-register of CS-pin
-- uint8_t cPin:            Portpin of CS-pin
-
-Return Value: none
-**********************************************/
-void mcpDACInit(volatile uint8_t *cPort, volatile uint8_t *cDDR, uint8_t cPin);
-        
-/**********************************************
- Public Function: setCSMCPADC
+ Public Function: mcpDACInit
  
- Purpose: set CS of MCPADC
+ Purpose: Initialise SPI interface
  
  Input Parameter:
  -volatile uint8_t *cPort:  Pointer to PORT-register of CS-pin
+ -volatile uint8_t *cDDR:   Pointer to DDR-register of CS-pin
  - uint8_t cPin:            Portpin of CS-pin
  
  Return Value: none
  **********************************************/
+void mcpDACInit(volatile uint8_t *cPort, volatile uint8_t *cDDR, uint8_t cPin);
+        
+/**********************************************
+Public Function: setCSMCPDAC
+     
+Purpose: set CS of MCPDAC
+     
+Input Parameter:
+-volatile uint8_t *cPort:  Pointer to PORT-register of CS-pin
+- uint8_t cPin:            Portpin of CS-pin
+     
+Return Value: none
+**********************************************/
 void setCSMCPDAC(volatile uint8_t *cPort, uint8_t cPin);
 
 /**********************************************
@@ -55,30 +55,32 @@ void setCSMCPDAC(volatile uint8_t *cPort, uint8_t cPin);
         
 void unSetCSMCPDAC(volatile uint8_t *cPort, uint8_t cPin);
 /**********************************************
- Public Function: spiSendByteMCPADC
+ Public Function: spiSendByteMCPDAC
  
- Purpose: send byte to MCPADC
+ Purpose: send byte to MCPDAC
  
  Input Parameter:
- -volatile uint8_t valueToSend:  value to send to MCPADC
+ -volatile uint8_t valueToSend:  value to send to MCPDAC
  
  Return Value: none
  **********************************************/
 void spiSendByteMCPDAC(uint8_t valueToSend);
         
 /**********************************************
- Public Function: getMCPADC
+ Public Function: setMCPDAC
  
- Purpose: get ADC value from designated channel
+ Purpose: set designated outputport to value with gain or deactivate/power down chip
  
  Input Parameter:
  -volatile uint8_t *cPort:  Pointer to PORT-register of CS-pin
  -uint8_t cPin:             Portpin of CS-pin
- -uint8_t channel:          designated channel
- -uint8_t mode:             0 for differential mode, 1 for single-ended mode
+ -uint8_t resolution:       Resolution of Chip in bit
+ -uint16_t  valueToSet:     value for output
+ -uint8_t channel:          outputport, 1 for port a, 2 for port b
+ -uint8_t gain:             gain for designated port, 1 for 1x, 2 for 2x
+ -uint8_t shutdown:         0 for activate chip, 1 for power down chip
  
- Return Value:
- -uint16_t returnValue:     ADC-value from designated channel
+ Return Value: none
  **********************************************/
 void setMCPDAC(volatile uint8_t *cPort, uint8_t cPin, uint8_t resolution, uint16_t valueToSet, uint8_t channel, uint8_t gain, uint8_t shutdown);
 
